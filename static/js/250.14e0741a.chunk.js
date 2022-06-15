4267dd88723f5496a85dd3a319d3dc5fc92a7fec
Kambale Kibeho <dessaikibeho@gmail.com>
2022-06-15 23:15:02 +0300

    Updates

--- static/js/250.14e0741a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksomapamoja_project=self.webpackChunksomapamoja_project||[]).push([[250],{7250:function(e,a,r){r.r(a);var t=r(2791),l=r(4498),c=(0,l.rule)({w:"100%","&>div":{pos:"relative",padb:"56.25%",h:0},"& iframe":{pos:"absolute",top:0,left:0,w:"100%",h:"100%"}});a.default=function(e){var a=e.id;return(0,e.renderWrap)(t.createElement("div",{className:c},t.createElement("div",null,t.createElement("iframe",{src:"https://jsfiddle.net/".concat(a,"/embedded/"),frameBorder:"0",allowFullScreen:!0}))))}}}]);
-//# sourceMappingURL=250.14e0741a.chunk.js.map
