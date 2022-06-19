88059befc78cd59f7a28ef0b258e711dc58cb5bd
lotiang <lotylobalu12@gmail.com>
2022-06-19 20:51:58 +0300

    :art: update-about-us-page

--- src/Pages/2.4Contact/Contact.js
+++ src/Pages/2.4Contact/Contact.js
@@ -4,20 +4,18 @@ import googlemap from '../../Images/googlemap.png'; // gives image path
 
 function Contact() {
     return (
-        <>
+        <div className="form">
     <label>
         <input id="name" type="text" placeholder="Name*" />
-     </label>
-
-     <label>
+     
         <input id="email" type="text" placeholder="Email*" />
-     </label>
+     
 
-      <label>
+      
          <input id="number" type="number" placeholder="Phone Number" />
-      </label>
+      
 
-      <label>
+      
          <input id="message" type="text" placeholder="Message" />
       </label>
 
@@ -27,7 +25,7 @@ function Contact() {
 
       <h2>Don't forget to leave a comment or a message </h2>
       
-</>
+</div>
     );
 }
 
